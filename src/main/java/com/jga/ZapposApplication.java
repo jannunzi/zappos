aac8a2b6efeb18be4bcab2b7833a2b8b4060062f
jannunzi <jannunzi@gmail.com>
2018-02-25 17:17:47 -0500

    first commit

---
+++ src/main/java/com/jga/ZapposApplication.java
@@ -0,0 +1,12 @@
+package com.jga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapposApplication.class, args);
+	}
+}
